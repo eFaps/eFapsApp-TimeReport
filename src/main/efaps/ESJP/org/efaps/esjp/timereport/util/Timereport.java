4fc4b9f5111ed91cd42d2185f6dbad5f3c955aa8
jose.mallqui@efaps.org <jose.mallqui@efaps.org@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-10-23 15:52:04 +0000

    - timereport: create configuration files on package util
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/timereport/trunk@10657 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/timereport/util/Timereport.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2003 - 2013 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.esjp.timereport.util;
+
+import java.util.UUID;
+
+import org.efaps.admin.common.SystemConfiguration;
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.util.cache.CacheReloadException;
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+@EFapsUUID("46e33969-a2cd-4416-bf20-cc75562d6be1")
+@EFapsRevision("$Rev$")
+public final class Timereport
+{
+    /**
+     * Singelton.
+     */
+    private Timereport()
+    {
+    }
+
+    /**
+     * @return the SystemConfigruation for Timereport
+     * @throws CacheReloadException on error
+     */
+    public static SystemConfiguration getSysConfig()
+        throws CacheReloadException
+    {
+        // Timereport-Configuration
+        return SystemConfiguration.get(UUID.fromString("bac3acca-6ec3-4be7-b190-4192638cbe27"));
+    }
+}
